08e533535154efab957f4d6f63894974d1b5c37f
OnlineAlex <becasualua@gmail.com>
2018-03-29 22:46:57 +0300

    Первая рабочая версия скрипта / заполнил Readme

--- README.md
+++ README.md
@@ -1,7 +1,54 @@
-# Sites Monitoring Utility
+# Утила мониторинга сатов
 
-[TODO. There will be project description]
+Скрипт проверяет работоспособность сайта и время до продления домена.
 
-# Project Goals
+* Если сервер дает ответ `200` - True, в других случая False
+* Если до конца аренды домена меньше 31 дня, вы получите False, в другом случае True
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+# Как работает 
+```bash
+$ python check_sites_health.py domains.txt
+ 
+Домен                          | Ответ сервера | Проплачено более |
+                                    HTTP 200          месяца
+https://amazon.com             |     False     |       True       |
+https://fbcdn.net              |     True      |       True       |
+https://windowsupdate.com      |     False     |       True       |
+https://mookie1.com            |     False     |      False       |
+https://rfihub.com             |     False     |       True       |
+......
+```
+# Требования
+Совестимые OC:
+* Linux,
+* Windows
+* MacOS
+
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5 выше
+
+И  пакетов из requirements.txt
+```bash
+pip install -r requirements.txt # или командой pip3
+```
+
+Список доменов должен быть в файле .txt. Каждый домен с новой строки.
+
+
+# Как запустить
+Стандатной командой `python` (на некоторых компьютерах `python3`)
+
+```bash
+$ python check_sites_health.py [-h] file_path
+
+positional arguments:
+  file_path   Адрес файла с доменами
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
+> Запуск для всех ОС одинаковый
+
+Помните, рекомендуется использовать [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) для лучшего управления пакетами.
+
+# Цели проекта
+Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DEVMAN.org](https://devman.org)
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,67 @@
+import argparse
+import os
+import requests
+from requests.exceptions import MissingSchema
+import whois
+from datetime import datetime
+import time
+
+
+def is_valid_file(argument_path):
+    if not os.path.exists(argument_path):
+        error_message = 'Путь {} не существует'.format(argument_path)
+        raise argparse.ArgumentTypeError(error_message)
+
+    return argument_path
+
+
 def load_urls4check(path):
-    pass
+    with open(path) as file_domains:
+        return file_domains.read().splitlines()
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        response = requests.get(url, timeout=5)
+        return bool(response.status_code == 200)
+    except (requests.ConnectionError, MissingSchema):
+        return False
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    while True:
+        try:
+            domain_whois = whois.whois(domain_name)
+            return domain_whois.expiration_date[0]
+        except TypeError:
+            return domain_whois.expiration_date
+        except ConnectionResetError:
+            time.sleep(5)
+            continue
+
+
+def is_expiration_date_more_mounth(expiration_date):
+    paid_term = expiration_date - datetime.now()
+    days_mounth = 31
+    return paid_term.days > days_mounth
+
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'file_path',
+        type=is_valid_file,
+        help='Адрес файла с доменами'
+    )
+    domains_file_path = parser.parse_args().file_path
+    domains = load_urls4check(domains_file_path)
+
+    print('\n{:<30} | Ответ сервера | Проплачено более |'.format('Домен'))
+    print('{:>44} {:>15}'.format('HTTP 200', 'месяца'))
+    for domain in domains:
+        domain_expiration_date = get_domain_expiration_date(domain)
+        print('{:<30} | {!s:^13} | {!s:^16} |'.format(
+            domain,
+            is_server_respond_with_200(domain),
+            is_expiration_date_more_mounth(domain_expiration_date)
+        ))
