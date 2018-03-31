c2a48a53310774ba4832c655f78f1f1e1da9c0d7
OnlineAlex <becasualua@gmail.com>
2018-03-31 10:35:22 +0300

    парсинг аргумента вынес в отдельную функцию / изменил логику в get_domain_expiration_date / в requirements.txt 2 библиотеки, который используются напрямую.

--- check_sites_health.py
+++ check_sites_health.py
@@ -14,6 +14,16 @@ def is_valid_file(argument_path):
     return argument_path
 
 
+def parsing_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'file_path',
+        type=is_valid_file,
+        help='Адрес файла с доменами'
+    )
+    return parser.parse_args()
+
+
 def load_urls4check(path):
     with open(path) as file_domains:
         return file_domains.read().splitlines()
@@ -32,14 +42,15 @@ def get_domain_expiration_date(domain_name):
     while attempt < 4:
         try:
             domain_whois = whois.whois(domain_name)
+            if type(domain_whois.expiration_date) is list:
+                return domain_whois.expiration_date[0]
+            else:
+                return domain_whois.expiration_date
         except ConnectionResetError:
             attempt += 1
             continue
-        if type(domain_whois.expiration_date) is list:
-            return domain_whois.expiration_date[0]
-        else:
-            return domain_whois.expiration_date
-    return False
+
+    return None
 
 
 def is_expiration_date_more_days(days, expiration_date):
@@ -49,21 +60,13 @@ def is_expiration_date_more_days(days, expiration_date):
 
 if __name__ == '__main__':
     min_paid_days = 31
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'file_path',
-        type=is_valid_file,
-        help='Адрес файла с доменами'
-    )
-    domains_file_path = parser.parse_args().file_path
+    domains_file_path = parsing_arguments().file_path
     domains = load_urls4check(domains_file_path)
 
     print('\n{:<30} | Ответ сервера | Проплачено более |'.format('Домен'))
     print('{:>44} {:>15}'.format('HTTP 200', 'месяца'))
     for domain in domains:
         domain_expiration_date = get_domain_expiration_date(domain)
-        if not domain_expiration_date:
-            exit('Удаленный хост принудительно разорвал существующее подключение')
         print('{:<30} | {!s:^13} | {!s:^16} |'.format(
             domain,
             is_server_respond_ok(domain),
